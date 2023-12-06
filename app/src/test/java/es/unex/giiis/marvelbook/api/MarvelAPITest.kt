bd758514b4fadae216f00c7535d7cb457a076305
Diego <dbautistf@alumnos.unex.es>
2023-12-06 13:14:26 +0100

    GD01-209 CU02 - IT2 - IMP - Test Components 2 #done #time 20m #comment Implement test for empty list of getPersonajes method.

--- app/src/test/java/es/unex/giiis/marvelbook/api/MarvelAPITest.kt
+++ app/src/test/java/es/unex/giiis/marvelbook/api/MarvelAPITest.kt
@@ -76,4 +76,50 @@ class MarvelAPITest {
 
         mockWebServer.shutdown()
     }
+
+
+    @Test
+    @Throws(IOException::class)
+    fun getEmptyPersonajesTest() = runBlocking {
+
+        val ejemploPersonajeDatos = PersonajeDatos(
+            offset = 0,
+            limit = 1,
+            total = 1,
+            count = 1,
+            results = ArrayList<PersonajeMarvel>()
+        )
+
+        val ejemploPersonajeCabecera = PersonajeCabecera(
+            code = 200,
+            status = "OK",
+            copyright = "Copyright © 2023 Marvel",
+            attributionText = "Data provided by Marvel. © 2023 MARVEL",
+            attributionHTML = "<a href=\"http://marvel.com\">Data provided by Marvel. © 2023 MARVEL</a>",
+            etag = "W/\"123456789\"",
+            data = ejemploPersonajeDatos
+        )
+        val gson = Gson()
+
+        val jsonString = gson.toJson(ejemploPersonajeCabecera)
+
+        val mockWebServer = MockWebServer()
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("").toString())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val mockedResponse = MockResponse()
+        mockedResponse.setResponseCode(200)
+        mockedResponse.setBody(jsonString)
+        mockWebServer.enqueue(mockedResponse)
+
+        val service: MarvelAPI = retrofit.create(MarvelAPI::class.java)
+
+        val response: PersonajeCabecera = service.getPersonajes(20)
+
+        TestCase.assertTrue(response.data!!.results.size == 0)
+
+        mockWebServer.shutdown()
+    }
 }
