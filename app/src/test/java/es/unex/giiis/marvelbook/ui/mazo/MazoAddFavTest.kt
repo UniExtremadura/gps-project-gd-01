77838bcd36121ca7773c5f511ec6494d363d5667
Alberto Crespo Murillo <acrespoq@alumnos.unex.es>
2023-12-12 19:08:08 +0100

    GD01-166 CU12 - IT5 - IMP - Test Components 2 #done #time 12m #comment Implement test for add a character to favorite list

--- app/src/test/java/es/unex/giiis/marvelbook/ui/mazo/MazoAddFavTest.kt
+++ app/src/test/java/es/unex/giiis/marvelbook/ui/mazo/MazoAddFavTest.kt
@@ -45,4 +45,39 @@ class MazoAddFavTest {
         Mockito.verify(personajeMazoDAO, times(1)).updatePersonajeMazo(modificarPersonajeMazo)
         Mockito.verify(personajeMazoDAO, times(1)).getById(1L)
     }
+
+    @SuppressLint("CheckResult")
+    @Test
+    fun addFavPersonaje2() {
+        //simular acciones
+        val personajeMazo = PersonajeMazo(1L, 1, "Spiderman", null, 1, 1, 1, 1, false)
+        doNothing().`when`(personajeMazoDAO).insertarPersonajeMazo(personajeMazo)
+
+        val personajeMazo2 = PersonajeMazo(2L, 1, "SpidermanAUX", null, 1, 1, 1, 1, false)
+        doNothing().`when`(personajeMazoDAO).insertarPersonajeMazo(personajeMazo2)
+
+        val modificarPersonajeMazo = PersonajeMazo(1L, 1, "Spiderman", null, 1, 1, 1, 1, true)
+        doNothing().`when`(personajeMazoDAO).updatePersonajeMazo(modificarPersonajeMazo)
+
+        `when`(personajeMazoDAO.getById(1L)).thenReturn(modificarPersonajeMazo)
+        `when`(personajeMazoDAO.getById(2L)).thenReturn(personajeMazo2)
+
+        //ejecutar las acciones
+        personajeMazoDAO.insertarPersonajeMazo(personajeMazo)
+        personajeMazoDAO.insertarPersonajeMazo(personajeMazo2)
+        personajeMazoDAO.updatePersonajeMazo(modificarPersonajeMazo)
+        val personajeResult = personajeMazoDAO.getById(1L)
+        val personajeResult2 = personajeMazoDAO.getById(2L)
+
+        //comprobar acciones
+        assertEquals(personajeResult.fav, true)
+        assertEquals(personajeResult2.fav, false)
+        Mockito.verify(personajeMazoDAO, times(1)).insertarPersonajeMazo(personajeMazo)
+        Mockito.verify(personajeMazoDAO, times(1)).insertarPersonajeMazo(personajeMazo2)
+        Mockito.verify(personajeMazoDAO, times(1)).updatePersonajeMazo(modificarPersonajeMazo)
+        Mockito.verify(personajeMazoDAO, times(1)).getById(1L)
+        Mockito.verify(personajeMazoDAO, times(1)).getById(2L)
+    }
+
+
 }
